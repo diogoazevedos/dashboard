19cd443fd5f650d1d11616524f9a765ba9fe123b
Donald Silveira <donaldsilveira@gmail.com>
2016-09-21 08:07:41 -0300

    feat: Create main component to internal views

---
+++ src/Main.vue
@@ -0,0 +1,21 @@
+<script>
+import AppNav from './components/Nav.vue'
+import AppFooter from './components/Footer.vue'
+export default {
+  components: {
+    AppNav, AppFooter
+  }
+}
+</script>
+
+<template>
+  <div>
+    <app-nav></app-nav>
+    <router-view></router-view>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<style lang='scss'>
+  body { padding-top: 50px; }
+</style>
