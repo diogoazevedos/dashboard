836a8748b4ad42f8c13268b8aa1be853a728fafb
Donald Silveira <donaldsilveira@gmail.com>
2016-09-21 08:05:43 -0300

    feat: Create dashboard and authentication routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,26 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import IndexView from '../views/IndexView.vue'
+import Main from '../Main.vue'
+import Dashboard from '../views/dashboard/Index.vue'
+import Signin from '../views/auth/Signin.vue'
+import Signup from '../views/auth/Signup.vue'
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: IndexView },
+    {
+      path: '/',
+      component: Main,
+      children: [
+        {
+          path: '',
+          component: Dashboard
+        }
+      ]
+    },
+    { path: '/signin', component: Signin },
+    { path: '/signup', component: Signup },
     { path: '*', redirect: '/' }
   ]
 })
