8afbc525b0d41c0fee2166e4efdd4be7ecdb9910
Donald Silveira <donaldsilveira@gmail.com>
2016-09-28 18:41:05 -0300

    refactor: Use Popup instead of Dropdown

---
+++ src/components/Popup.vue
@@ -0,0 +1,57 @@
+<script>
+import Popup from 'tether-drop'
+
+export default {
+  props: {
+    trigger: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: String,
+      default: 'bottom left'
+    }
+  },
+
+  data () {
+    return {
+      popup: null
+    }
+  },
+
+  mounted () {
+    this.popup = new Popup({
+      target: this.$parent.$refs[this.trigger],
+      content: this.$refs.popup,
+      position: this.position,
+      openOn: 'click'
+    })
+  }
+}
+</script>
+
+<template>
+  <div ref="popup" class="popup dropdown-menu border-radius-s">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang='scss'>
+.drop-open {
+  .drop-after-open {
+    opacity: 1;
+    pointer-events: auto;
+  }
+}
+
+.drop {
+  opacity: 0;
+  pointer-events: none;
+  transition: opacity ease 0.1s;
+
+  .popup {
+    margin-top: 0;
+    transition: margin ease 0.1s;
+  }
+}
+</style>
