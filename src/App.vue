6d9122dc53f13cbad7c7df087ed84dd0575df167
Donald Silveira <donaldsilveira@gmail.com>
2016-09-28 18:43:13 -0300

    refactor: Remove unused backdrop element

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <main>
     <router-view></router-view>
-    <div class="app-backdrop fixed-top fixed-bottom"></div>
   </main>
 </template>
 
 <style lang="scss">
 $simple-line-font-path: "~simple-line-icons/fonts/";
-@import '~voyager-assets/scss/voyager';
 @import '~simple-line-icons/scss/simple-line-icons';
-
-.app-backdrop {
-  opacity: 0;
-  pointer-events: none;
-  z-index: 999;
-}
+@import './assets/scss/voyager';
 </style>
